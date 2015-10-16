65584fb29b6c964c6a3c0f2d8ac8e60441aa3ec9
Your Name <you@example.com>
2015-10-16 02:17:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # codemirror-gherkin-linter
+
+A gherkin linter for codemirror.
+
+
+Gherkin3 js is included, and you can see test.html for a test version.
---
+++ gherkin-lint.js
@@ -0,0 +1,38 @@
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+// Depends on jsonlint.js from https://github.com/zaach/jsonlint
+
+// declare global: jsonlint
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+"use strict";
+
+CodeMirror.registerHelper("lint", "gherkin", function(text) {
+  var found = [];
+  var parser = new Gherkin.Parser();
+      // $.each(val, function(i,line){
+  try { parser.parse(text); } 
+  catch (e) { 
+    for (var err in e.errors){
+      err = e.errors[err];
+      var loc = err.location;
+      found.push({
+        from: CodeMirror.Pos(loc.line - 1, loc.column),
+        to: CodeMirror.Pos(loc.line - 1, loc.column),
+        message: err.message
+      });
+    }
+  }
+  
+  return found;
+});
+
+});
